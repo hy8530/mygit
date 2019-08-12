dfb4e4c5b763a02b04a45edcffc45324b4fbd435
huyu <123@qq.com>
2019-08-12 19:22:15 +0800

    取回user

--- healthy_parent/healthy_web/src/main/java/com/itheima/security/SpringSecurityUserService.java
+++ healthy_parent/healthy_web/src/main/java/com/itheima/security/SpringSecurityUserService.java
@@ -3,7 +3,6 @@ package com.itheima.security;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.itheima.pojo.Permission;
 import com.itheima.pojo.Role;
-import com.itheima.pojo.User;
 import com.itheima.service.UserService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
